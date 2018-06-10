168c0aae23e45262c92e80a518ac8f125bea26ae
hectorfgf <tito_lp_13@hotmail.com>
2018-06-10 20:17:19 +0100

    Cambios en control de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import {LoginPage} from "../pages/login/login";
+import {JwtHelper} from "angular2-jwt";
 
 @Component({
   templateUrl: 'app.html'
@@ -16,5 +17,17 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    if (localStorage.getItem('token')) {
+      const decodeToken = new JwtHelper().decodeToken(localStorage.getItem('token'));
+      if(decodeToken.rol === 'progenitor'){
+        this.rootPage = 'TabsPage';
+      }else if(decodeToken.rol==='teacher'){
+        this.rootPage = 'HomeProfesorPage';
+      }
+      this.rootPage = 'TabsPage';
+    } else {
+      this.rootPage = 'LoginPage';
+    }
   }
 }
--- src/providers/api-route.ts
+++ src/providers/api-route.ts
@@ -1,4 +1,4 @@
 export const apiURL = 'http://hermerestbackend.ciber.ulpgc.es/';
-//export const apiURL = 'http://localhost/hermerest_backend/web/';
+//export const apiURL = 'http://localhost/hermerest_backend/web/app_dev.php/';
 //export const apiURL = 'http://hermerest.ciber.ulpgc.es/';
 
